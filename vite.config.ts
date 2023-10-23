7f3fa7b1f7ae17f093541be5813104ec27e30ded
Örsan Günay <oersan.guenay@sportscheck.com>
2023-10-23 22:25:29 +0200

    Update docker setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: false,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
 })
