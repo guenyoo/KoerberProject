40b1f7a12a8237bbe5c6aa8b8cd8f12eccd00892
Örsan Günay <oersan.guenay@sportscheck.com>
2023-10-23 22:03:27 +0200

    Retrieve user-agent

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,10 @@
 import * as http from 'http';
 
 const PORT = 3000;
-console.log('something 2');
-const server = http.createServer((_, res) => {
+
+const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log(req.headers['user-agent']);
   res.end('Hello, TS HTTP Server\n');
 })
 
