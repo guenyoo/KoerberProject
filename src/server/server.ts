4c59a3c6450f3c9a237732aea56c318cdb2b6d6b
Örsan Günay <oersan.guenay@sportscheck.com>
2023-10-26 16:32:23 +0200

    Remove log and cbf

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,10 +3,9 @@ import * as http from 'http';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log(req.headers['user-agent']);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, Docker TS HTTP Server\n');
-})
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
