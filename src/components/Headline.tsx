e1c58fc426e8aae8928b57ca3877f859ca1a5515
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-11 14:49:42 +0100

    Add className to HeadlineComp

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,11 +1,12 @@
 interface HeadlineProps {
   type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   content: string;
+  className?: string;
 }
 
-const Headline = ({ type, content }: HeadlineProps) => {
+const Headline = ({ type, content, className }: HeadlineProps) => {
   const Comp = type;
-  return <Comp>{content}</Comp>;
+  return <Comp className={className}>{content}</Comp>;
 };
 
 export { Headline };
