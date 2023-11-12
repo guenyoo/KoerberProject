8412324cb3694af008a0bf4b8435c4e517c62e1a
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 15:40:56 +0100

    Add a sort function to Store

--- src/stores/devices-store.ts
+++ src/stores/devices-store.ts
@@ -4,11 +4,27 @@ import { type Device } from '@/components/Devices';
 interface DevicesState {
   devices: Device[];
   addDevice: (newItem: Device) => void;
+  removeDevice: (deviceToRemove: Device) => void;
+  sortBy: (value: keyof Device) => void;
 }
 
 const devicesStore = create<DevicesState>((set) => ({
   devices: [],
   addDevice: (newItem: Device) => set((state) => ({ devices: [...state.devices, newItem] })),
+  removeDevice: (deviceToRemove: Device) =>
+    set((state) => ({ devices: state.devices.filter((device) => device.id !== deviceToRemove.id) })),
+  sortBy: (value: keyof Device) =>
+    set((state) => ({
+      devices: [...state.devices].sort((a: Device, b: Device) => {
+        if (typeof a[value] === 'string' && typeof b[value] === 'string')
+          return (a[value] as string).localeCompare(b[value] as string, undefined, {
+            numeric: true,
+            sensitivity: 'base',
+          });
+        if (typeof a[value] === 'number' && typeof b[value] === 'number') return Number(a[value]) - Number(b[value]);
+        return 0;
+      }),
+    })),
 }));
 
 // Async function to fetch devices and update the store state
@@ -16,8 +32,6 @@ const fetchDevices = async () => {
   try {
     const response = await fetch('http://localhost:3000/api/devices');
     const devicesData = await response.json();
-
-    console.log(devicesData);
 
     devicesStore.setState({ devices: devicesData });
   } catch (error) {
