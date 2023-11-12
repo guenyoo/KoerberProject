1ea329575761fb954d06858a94b0d8298acc8073
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 18:52:42 +0100

    Add config file for API Locations

---
+++ src/config/apis.ts
@@ -0,0 +1,7 @@
+const API = {
+  ADD: 'http://localhost:3000/api/devices/add',
+  DELETE: 'http://localhost:3000/api/devices/delete',
+  PUT: 'http://localhost:3000/api/devices/edit-device',
+};
+
+export { API };
