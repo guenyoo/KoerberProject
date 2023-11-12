94c56e77336a5c2152aaff3075f8aa359d59ce88
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 20:03:33 +0100

    Use .env Attributes

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // Create a real user instead of root
-  // password: '', // And give that real user a real password
-  database: 'devices',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'devices',
 });
 
 const endConnection = () => pool.end();
