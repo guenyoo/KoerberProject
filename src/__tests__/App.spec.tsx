7e8fe01a09150442ddb7189085d7077ff1732b66
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 23:03:52 +0100

    Write first meaningful Test

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,13 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { devicesStore } from '@/stores/devices-store';
 
 import App from '@/App';
 
 describe('App', () => {
-  it('renders headline', () => {
-    render(<App />);
+  beforeEach(() => {
+    devicesStore.setState({
+      devices: [
+        {
+          id: 0,
+          deviceName: 'Hans Rüdiger',
+          deviceType: 'Smartphone',
+          ownerName: 'Klaus Herbert',
+          batteryStatus: 0,
+        },
+      ],
+    });
+  });
 
-    screen.debug();
-
-    // check if App components renders headline
+  it('renders App with Hans Rüdiger as solo Device', () => {
+    const app = render(<App />);
+    expect(screen.getByText('Hans Rüdiger')).toBeTruthy();
+    expect(app).toMatchSnapshot();
   });
 });
