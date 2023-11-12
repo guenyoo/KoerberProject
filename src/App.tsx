ba7298e38475c3306cc7385cac3364f68bdd9f20
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 13:06:40 +0100

    Implement Routes for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-// import { useState } from 'react';
-// import reactLogo from '@/assets/react.svg';
-// import viteLogo from '/vite.svg';
 import './App.css';
 import { Headline } from '@/components/Headline';
 import { Devices } from './components/Devices';
 import { AddDevice } from './components/AddDevice';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { FileNotFound } from './views/FileNotFound';
 
 function App() {
   return (
     <>
-      <Headline type="h1" content="Device Manager" className="text-3xl font-bold" />
-      <Devices className="my-3" />
-      <AddDevice />
+      <Router>
+        <header className="text-left dark:bg-slate-500 w-full p-4 text-slate-800 flex justify-between items-center">
+          <Headline type="h1" content="Device Manager" className="text-3xl font-bold" />
+          <nav className="flex">
+            <ul className="list-none flex flex-wrap gap-2">
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'font-bold dark:text-slate-700' : 'dark:text-slate-700')}
+                  to="/">
+                  Devices
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? 'font-bold dark:text-slate-700' : 'dark:text-slate-700')}
+                  to="/add-devices">
+                  Add Devices
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <section className="p-4">
+          <Routes>
+            <Route path="/" element={<Devices />} />
+            <Route path="/add-devices" element={<AddDevice />} />
+            <Route path="*" element={<FileNotFound />} />
+          </Routes>
+        </section>
+      </Router>
     </>
   );
 }
