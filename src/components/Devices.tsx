cb080623515be1b2fd0afd97147fdbadd8317c19
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 16:04:16 +0100

    Add sortHandler in Devices.tsx

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -18,15 +18,38 @@ interface DevicesProps {
   className?: string;
 }
 
+function isValidSortKey(value: string): value is keyof Device {
+  return (
+    value === 'id' ||
+    value === 'deviceName' ||
+    value === 'deviceType' ||
+    value === 'ownerName' ||
+    value === 'batteryStatus'
+  );
+}
+
 const Devices = ({ className }: DevicesProps) => {
   const devices = devicesStore((state) => state.devices);
   const removeDevice = devicesStore((state) => state.removeDevice);
   const [selectedDevice, setSelectedDevice] = useState<Device | null>(null);
   const setSortBy = devicesStore((state) => state.sortBy);
+  const [sortLabel, setSortLabel] = useState('');
+  const [sortDirection, setSortDirection] = useState(true);
 
   const handleDeviceEdit = (device: Device) => {
     if (selectedDevice && selectedDevice.id === device.id) setSelectedDevice(null);
     else setSelectedDevice(device);
+  };
+
+  const sortHandler = (label: string) => {
+    if (isValidSortKey(label)) {
+      if (sortLabel === label) {
+        setSortDirection((state) => !state);
+      } else if (sortLabel && sortLabel !== label) setSortDirection(true);
+      else setSortDirection(false);
+      setSortLabel(label);
+      setSortBy(label, sortDirection);
+    } else console.error('Invalid sort key:', label);
   };
 
   const handleDeviceDump = (device: Device) => {
@@ -41,8 +64,6 @@ const Devices = ({ className }: DevicesProps) => {
     }
   };
 
-  console.log(devices);
-
   // TODO: Implement also filtering for i.e. devices that have no more battery
   // or add a searchbar that filters for the supplied name of the Owner
 
@@ -54,16 +75,16 @@ const Devices = ({ className }: DevicesProps) => {
         <ul className="list-none flex gap-2">
           {/* map over these */}
           <li>
-            <button onClick={() => setSortBy('batteryStatus')}>By Battery Status</button>
+            <button onClick={() => sortHandler('batteryStatus')}>By Battery Status</button>
           </li>
           <li>
-            <button onClick={() => setSortBy('ownerName')}>By Owner</button>
+            <button onClick={() => sortHandler('ownerName')}>By Owner</button>
           </li>
           <li>
-            <button onClick={() => setSortBy('deviceName')}>By Name</button>
+            <button onClick={() => sortHandler('deviceName')}>By Name</button>
           </li>
           <li>
-            <button onClick={() => setSortBy('deviceType')}>By Type</button>
+            <button onClick={() => sortHandler('deviceType')}>By Type</button>
           </li>
         </ul>
       </div>
@@ -74,11 +95,16 @@ const Devices = ({ className }: DevicesProps) => {
               'text-left bg-lime-200 rounded-xl p-8 dark:border-slate-800 dark:bg-slate-700 shadow-lg relative border border-transparent hover:border-solid dark:hover:border-slate-400',
               selectedDevice && selectedDevice?.id !== device.id ? 'opacity-50' : '',
             ].join(' ')}
-            key={device.id}>
+            key={device.id}
+          >
             <div className="w-5/6">
               <p className="mb-4">
                 Device Name: <br />
                 <strong>{device.deviceName}</strong>
+              </p>
+              <p className="mb-4">
+                Device Type: <br />
+                <strong>{device.deviceType}</strong>
               </p>
               <p className="mb-4">
                 Owner Name: <br />
@@ -92,12 +118,14 @@ const Devices = ({ className }: DevicesProps) => {
             <div className="bg-slate-600 w-1/6 absolute top-0 right-0 bottom-0 rounded-r-xl flex flex-wrap justify-center items-center py-2">
               <button
                 className={['bg-transparent group', selectedDevice?.id === device.id ? 'bg-slate-100' : ''].join(' ')}
-                onClick={() => handleDeviceEdit(device)}>
+                onClick={() => handleDeviceEdit(device)}
+              >
                 <PencilIcon className="w-4 text-slate-800 group-hover:text-slate-400" />
               </button>
               <button
                 className={['bg-transparent group', selectedDevice?.id === device.id ? 'bg-slate-100' : ''].join(' ')}
-                onClick={() => handleDeviceDump(device)}>
+                onClick={() => handleDeviceDump(device)}
+              >
                 <TrashIcon className="w-4 text-slate-800 group-hover:text-slate-400" />
               </button>
             </div>
