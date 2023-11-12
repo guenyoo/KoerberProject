99adba3bbecc21dff26bf5253e59079f154dbe8e
Örsan Günay <oersan.guenay@sportscheck.com>
2023-11-12 13:08:13 +0100

    In Devices, use devices Store

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -1,6 +1,6 @@
 import { z } from 'zod';
-import { useEffect, useState } from 'react';
 import { Headline } from './Headline';
+import { devicesStore } from '@/stores/devices-store';
 
 const DeviceSchema = z.object({
   id: z.number(),
@@ -17,28 +17,23 @@ interface DevicesProps {
 }
 
 const Devices = ({ className }: DevicesProps) => {
-  const [devices, setDevices] = useState<Device[]>([]);
-
-  useEffect(() => {
-    const fetchDevicesInitially = () => fetch('http://localhost:3000/api/devices');
-    fetchDevicesInitially()
-      .then((data) => data.json())
-      .then((data) => setDevices(data))
-      .catch((e) => new Error(e));
-  }, []);
+  const devices = devicesStore((state) => state.devices);
 
   return (
     <div className={className}>
       <Headline type="h2" content="Existing Devices" className="font-bold text-2xl mb-4" />
-      <ul className="list-none flex flex-wrap gap-4">
+      <ul className="list-none grid grid-cols-3 gap-4">
         {devices.map((device) => (
           <li className="text-left bg-lime-200 rounded-xl p-8 dark:bg-slate-700 shadow-lg" key={device.id}>
-            <div>
+            <p>
               Device Name: <b>{device.deviceName}</b>
-            </div>
-            <div>
+            </p>
+            <p>
+              Owner Name: <b>{device.ownerName}</b>
+            </p>
+            <p>
               BatteryStatus: <b>{device.batteryStatus} %</b>
-            </div>
+            </p>
           </li>
         ))}
       </ul>
